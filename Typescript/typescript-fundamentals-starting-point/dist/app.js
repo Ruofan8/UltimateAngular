dcabd20e1d39f486d9080e4b324e8e5bf9684768
Ruofan Wang <r.wang@brunel.net>
2017-06-29 16:51:30 +0200

    6

--- Typescript/typescript-fundamentals-starting-point/dist/app.js
+++ Typescript/typescript-fundamentals-starting-point/dist/app.js
@@ -1,10 +1,12 @@
 "use strict";
-function generateRandomId(symbal, length) {
-    return symbal + Math.random().toString(36).substr(2, length);
+function generateRandomId(options) {
+    return options.symbal + Math.random().toString(36).substr(2, options.length);
 }
 function main() {
     var appComponent = document.getElementById('app');
     setInterval(function () {
-        appComponent.innerHTML = generateRandomId('#', 7);
+        if (appComponent) {
+            appComponent.innerHTML = generateRandomId({ symbal: '#', length: 7 });
+        }
     }, 1000);
 }
--- Typescript/typescript-fundamentals-starting-point/src/app.ts
+++ Typescript/typescript-fundamentals-starting-point/src/app.ts
@@ -1,12 +1,18 @@
 type validSymbal = '#' | '@' | '$'; 
 
-function generateRandomId( symbal:validSymbal, length:number ) {
-    return symbal + Math.random().toString(36).substr(2, length)
+interface config {
+    symbal:validSymbal, 
+    length:number
+}
+function generateRandomId( options:config ) :string {
+    return options.symbal + Math.random().toString(36).substr(2, options.length)
 }
 
 function main() {
-    var appComponent = document.getElementById('app')
+    var appComponent = document.getElementById('app');
     setInterval(function() {
-        appComponent.innerHTML = generateRandomId('#', 7);
+        if(appComponent) {
+            appComponent.innerHTML = generateRandomId({ symbal:'#', length: 7});
+        }
     }, 1000)
 }
